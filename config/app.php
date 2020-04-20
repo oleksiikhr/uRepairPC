088c497ef777e185bc4f77231f4c40ddcdf682eb
Alexey Khrushch <alexeykhr@outlook.com>
2020-04-20 18:24:34 +0300

    chore: add rabbitmq

--- config/app.php
+++ config/app.php
@@ -239,6 +239,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
         'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Amqp' => Bschmitt\Amqp\Facades\Amqp::class,
 
     ],
 
