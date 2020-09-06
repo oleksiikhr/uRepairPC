be85df2224038b6567ab11bc0326b8f911ee1b4d
Alexey Khrushch <alexeykhr@outlook.com>
2020-09-06 17:57:03 +0300

    chore(test): correct basic feature test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -13,7 +13,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest(): void
     {
-        $response = $this->get('/');
+        $response = $this->get('/api/settings/global');
 
         $response->assertStatus(200);
     }
